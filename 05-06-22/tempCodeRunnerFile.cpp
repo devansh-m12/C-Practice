d99194f5d854f59bf5d72f77198c785e2fbb1ec0
devansh-m12 <20bcs039@iiitdwd.ac.in>
2022-06-06 15:25:38 +0530

    commit

---
+++ 05-06-22/tempCodeRunnerFile.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int n,x;
+    cin>>n>>x;
+
+    if(n%2 == 0){
+        while(n !=0){
+        if(n = x){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    }
+    else{
+        int z = x%2;
+        if ((x<=n)(n%2 = x%2)){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
